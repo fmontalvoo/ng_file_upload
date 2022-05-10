2d676d076faeab422c4f00bb496f4e9684fdb59d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-10 09:18:04 -0500

    Se crean componentes y rutas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhotosComponent } from './components/photos/photos.component';
+import { UploadComponent } from './components/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'photos',
+    component: PhotosComponent,
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'photos',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent,
+    UploadComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
