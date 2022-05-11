64c437ee70fd13fbbcb9ed272c3a7bf25c83d556
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-11 09:11:18 -0500

    Definicion de validaciones para archivos de imagen.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/directives/drag-file.directive.ts
+++ src/app/directives/drag-file.directive.ts
@@ -7,11 +7,14 @@ import {
   Output
 } from '@angular/core';
 
+import { ImageModel } from '../models/image.model';
+
 @Directive({
   selector: '[dragFile]'
 })
 export class DragFileDirective {
 
+  @Input() private images: ImageModel[] = [];
   @Output() private onMouseOver: EventEmitter<boolean>;
 
   constructor() {
@@ -26,6 +29,24 @@ export class DragFileDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.onMouseOver.emit(false);
+  }
+
+  // Validaciones
+  private fileCanBeUploaded(file: File): boolean {
+    return this.isImage(file.type) && !this.imageAlreadyExists(file.name);
+  }
+
+  private preventBehavior(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private imageAlreadyExists(name: string): boolean {
+    return !!this.images.find(image => image.fileName === name);
+  }
+
+  private isImage(type: string): boolean {
+    return !!type && type.startsWith('image/');
   }
 
 }
