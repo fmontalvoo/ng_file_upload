106c095e9d3ba9865d506da5657939b44fb42a9e
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-11 10:11:30 -0500

    Se envia los archivos de imagen a firebase para su almacenamiento.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -4,6 +4,7 @@ import { ImageModel } from '../models/image.model';
 
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +13,35 @@ export class UploadFilesService {
 
   private IMAGE_FOLDER = 'images';
 
-  constructor(private af: AngularFirestore, private afs: AngularFireStorage) {
+  constructor(private afs: AngularFirestore, private storage: AngularFireStorage) {
 
   }
 
   public saveImage(image: { name: string, url: string }) {
-    return this.af.collection(this.IMAGE_FOLDER).add(image);
+    return this.afs.collection(this.IMAGE_FOLDER).add(image);
   }
 
   public uploadImages(images: ImageModel[]) {
-    console.log('uploadImages', images);
+    for (const image of images) {
+      image.isUploading = true;
+      if (image.progress >= 100)
+        continue;
+
+      const path = `${this.IMAGE_FOLDER}/${image.fileName}`;
+      const task = this.storage.upload(path, image.file);
+      const ref = this.storage.ref(path);
+
+      task.percentageChanges()
+        .subscribe(progress => progress ? image.progress = progress : 0);
+
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          const downloadURL = ref.getDownloadURL()
+          downloadURL.subscribe(url => (image.url = url,
+            image.isUploading = false,
+            this.saveImage({ name: image.fileName, url })));
+        })
+      ).subscribe();
+    }
   }
 }
